68b25663d40e9ea4e5c8d56ea4b1f466c019dc5b
websites <boghack19@gmail.com>
2026-03-03 11:32:14 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyACtwAXFJGewPZyQmOlpdVm-uchVrYW_n0",
+    authDomain: "webbuilder-pro.firebaseapp.com",
+    projectId: "webbuilder-pro",
+    storageBucket: "webbuilder-pro.firebasestorage.app",
+    messagingSenderId: "4905721247",
+    appId: "1:4905721247:web:386697a11ccf4cbbc0f1aa"
+};
